66ad83fd15cc0a47fd663cb6cf762cef05eb81f1
Evan Buss <evan.buss28@gmail.com>
2018-02-23 15:07:29 -0500

    Updated

--- download.sh
+++ download.sh
@@ -3,20 +3,20 @@
 
 echo "Removing old folders..."
 rm -r .conky
-rm -r .vim
-rm -r .oh-my-zsh
+# rm -r .vim
+# rm -r .oh-my-zsh
 
 echo "Copying .vimrc to home directory..."
 cp .vimrc ~/.vimrc
 
-echo "Copying .vim folder to home directory..."
-cp -a .vim ~/.vim
+# echo "Copying .vim folder to home directory..."
+# cp -a .vim ~/.vim
 
 echo "Copying .zshrc to home directory..."
 cp .zshrc ~/.zshrc 
 
-echo "Copying .oh-my-zsh folder to home directory..."
-cp -a .oh-my-zsh ~/.oh-my-zsh
+# echo "Copying .oh-my-zsh folder to home directory..."
+# cp -a .oh-my-zsh ~/.oh-my-zsh
 
 echo "Copying .tmux.conf to home directory..."
 cp .tmux.conf ~/.tmux.conf
