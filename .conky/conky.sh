3f3eaff37e3df0cb50e8bf5984dfe764f7a0e017
Evan Buss <evan.buss28@gmail.com>
2018-02-23 15:20:48 -0500

    Updated conky theme

--- .conky/conky.sh
+++ .conky/conky.sh
@@ -5,4 +5,6 @@ sleep 5
 
 # conky -q -c ~/.conky/.conkyrc-minimal & exit
 
-conky -q -c ~/.conky/.conkyrc-cpu4ram & exit
+# conky -q -c ~/.conky/.conkyrc-cpu4ram & exit
+
+conky -q -c ~/.conky/.conkyrc-minimal & exit
