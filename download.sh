010910cfc67f0e48b1b90ed9ef9e2c04f45ac7ab
Evan Buss <evan.buss28@gmail.com>
2018-02-23 14:46:28 -0500

    Added vim and zsh folders

--- download.sh
+++ download.sh
@@ -1,13 +1,25 @@
 #!/bin/bash
 #copies files from git repo to their home directories
+
+echo "Removing old folders..."
+rm -r .conky
+rm -r .vim
+rm -r .oh-my-zsh
+
 echo "Copying .vimrc to home directory..."
 cp .vimrc ~/.vimrc
 
+echo "Copying .vim folder to home directory..."
+cp -a .vim ~/.vim
+
 echo "Copying .zshrc to home directory..."
 cp .zshrc ~/.zshrc 
+
+echo "Copying .oh-my-zsh folder to home directory..."
+cp -a .oh-my-zsh ~/.oh-my-zsh
 
 echo "Copying .tmux.conf to home directory..."
 cp .tmux.conf ~/.tmux.conf
 
 echo "Copying .conky to home directory..."
-cp -r .conky ~/.conky
+cp -a .conky ~/.conky
--- upload.sh
+++ upload.sh
@@ -1,13 +1,33 @@
 #!/bin/bash
 #copies files from directory to this git repo
+echo "Removing old files from repo directory"
+rm .vimrc
+rm -r .vim
+
+rm .zshrc
+rm .zsh_history
+rm -r .oh-my-zsh
+
+rm .tmux.conf
+rm -r .conky
+
 echo "Copying .vimrc to repo..."
 cp ~/.vimrc .vimrc
+
+echo "Copying .vim folder to repo"
+cp -a ~/.vim .vim
 
 echo "Copying .zshrc to repo..."
 cp ~/.zshrc .zshrc
 
+echo "Copying .zsh_history to repo"
+cp ~/.zsh_history .zsh_history
+
+echo "Copying .oh-my-zsh folder to repo..."
+cp -a ~/.oh-my-zsh .oh-my-zsh
+
 echo "Copying .tmux.conf to repo..."
 cp ~/.tmux.conf .tmux.conf
 
-echo "Copying .conky to repo..."
-cp -r ~/.conky .conky
+echo "Copying .conky folder to repo..."
+cp -a ~/.conky .conky
