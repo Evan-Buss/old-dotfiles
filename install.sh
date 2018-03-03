e906a4ea022c64814a7dcefa2e0b584f2f05fa71
Evan Buss <evan.bus28@gmail.com>
2018-03-03 12:39:45 -0500

    added new dotfiles and updated script

--- install.sh
+++ install.sh
@@ -5,24 +5,37 @@ echo "Removing old folders..."
 rm -r ~/.conky
 rm -r ~/.config/polybar
 rm -r ~/.config/i3
+rm -r ~/.config/rofi
+rm -r ~/.config/neofetch
 # rm -r .vim
 # rm -r .oh-my-zsh
+
+echo "Copying i3 folder to .config directory..."
 cp -a i3 ~/.config
+
+echo "Copying polybar folder to .config directory..."
 cp -a polybar ~/.config
+
+echo "Copying compton.conf to .config directory..."
+cp compton.conf ~/.config
+
+echo "Copying neofetch folder to .config directory..."
+cp -a neofetch ~/.config
+
 echo "Copying .vimrc to home directory..."
 cp .vimrc ~/.vimrc
 
-# echo "Copying .vim folder to home directory..."
-# cp -a .vim ~/.vim
-
 echo "Copying .zshrc to home directory..."
 cp .zshrc ~/.zshrc
-
-# echo "Copying .oh-my-zsh folder to home directory..."
-# cp -a .oh-my-zsh ~/.oh-my-zsh
 
 echo "Copying .tmux.conf to home directory..."
 cp .tmux.conf ~/.tmux.conf
 
 echo "Copying .conky to home directory..."
 cp -a .conky ~/.conky
+
+# echo "Copying .oh-my-zsh folder to home directory..."
+# cp -a .oh-my-zsh ~/.oh-my-zsh
+
+# echo "Copying .vim folder to home directory..."
+# cp -a .vim ~/.vim
--- update-repo.sh
+++ update-repo.sh
@@ -1,21 +1,23 @@
 #!/bin/bash
 #copies files from directory to this git repo
-echo "Removing old files from repo directory"
-#rm .vimrc
+echo "Removing old folders from repo directory..."
 rm -r i3
 rm -r polybar
-#rm .zshrc
-#rm .zsh_history
-#rm .tmux.conf
 rm -r .conky
+rm -r neofetch
+rm -r rofi
 
+echo "Copying polybar from .config directory..."
 cp -a ~/.config/polybar polybar
+
+echo "Copying i3 from .config directory..."
 cp -a ~/.config/i3 i3
+
+echo "Copying compton.conf from .config directory..."
+cp ~/.config/compton.conf compton.conf
+
 echo "Copying .vimrc to repo..."
 cp ~/.vimrc .vimrc
-
-# echo "Copying .vim folder to repo"
-# cp -a ~/.vim .vim
 
 echo "Copying .zshrc to repo..."
 cp ~/.zshrc .zshrc
@@ -23,11 +25,14 @@ cp ~/.zshrc .zshrc
 echo "Copying .zsh_history to repo"
 cp ~/.zsh_history .zsh_history
 
-# echo "Copying .oh-my-zsh folder to repo..."
-# cp -a ~/.oh-my-zsh .oh-my-zsh
-
 echo "Copying .tmux.conf to repo..."
 cp ~/.tmux.conf .tmux.conf
 
 echo "Copying .conky folder to repo..."
 cp -a ~/.conky .conky
+
+# echo "Copying .vim folder to repo"
+# cp -a ~/.vim .vim
+
+# echo "Copying .oh-my-zsh folder to repo..."
+# cp -a ~/.oh-my-zsh .oh-my-zsh
